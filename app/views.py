db758c8372f7683bf91ae7c479c97401aad836b2
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-10-12 10:47:19 +0300

    This will add the search input field in navbar

--- app/views.py
+++ app/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Image, Location, Category
 #My views
-
-def welcome(request):
-    return render(request, 'base.html')
 
 def home(request):
     return render(request, 'index.html')
