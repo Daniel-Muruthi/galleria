2d3d7ceda28e1c8d614a533b0b5bc03c645edbab
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-10-12 15:00:10 +0300

    This will add the update an delete functions

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url(r'^$', views.home, name='homepage')
+    url(r'^$', views.home, name='homepage'),
+
 ]
 
 if settings.DEBUG:
--- galleria/settings.py
+++ galleria/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap3',
+    'fontawesomefree',
     'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
